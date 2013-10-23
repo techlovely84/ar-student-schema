99904554ea6c08ce5f7d74becbbb1f29d7b0ca00
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-23 17:54:00 -0500

    Finished the challenge.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,19 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  validates :email, :format => { :with => /\A.+@.+\..{2,}\z/, :message => 'Invalid email format.' }, :uniqueness => true
+  validates :age, :exclusion => { :in => (0..5).to_a}
+  validates :phone, :format => { :with => /(\d{3}).*(\d{3}).*(\d{4})/, :message => 'Invalid phone number format.' }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    now = Date.today
+    now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
+
 end
+
